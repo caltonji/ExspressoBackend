9be0b5ea3a241e5bb5a2d53082b2e6bd12df9867
Mitch Webster <mitchell.a.webster@gmail.com>
2015-09-26 01:12:50 -0400

    token auth

--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,34 @@
 /**
  * Created by Mitch Webster on 9/26/2015.
  */
-var User = require('../../models/user');
+var User = require('../models/user');
+var hashing = require('../../config/hash');
+var jwt = require('jsonwebtoken');
+var token_config = require('../../config/token');
 
 exports.login = function(req, res, next) {
-    if (!req.body) {
+    if (!req.body || !req.body.email || !req.body.password) {
         res.json({error: true, body: "Invalid Request"});
-    } else if (!req.body.email) {
-        res.json({error: true, body: "Email is required"});
-    } else if (!req.body.password) {
-        res.json({error: true, body: "Password is required"});
     } else {
-        User.find({email: req.body.email}, '-_id email passwordHash status access.admin access.clean access.body access.phil access.fines access.referrals',function (err, users) {
+        User.find({email: req.body.email}, '_id email passwordHash status access.admin access.clean access.body access.phil access.fines access.referrals', function (err, users) {
+
+
+            firstName: {type: String, required: true},
+            lastName: {type: String, required: true},
+            email: {type: String, required: true},//must be a GT email
+            phoneNumber: {type: String, required: true},
+            passwordHash: {type: String, required: true},
+            passwordSalt: {type: String, required: true},
+            currentRating: {type: Number, required: true},
+            amountOfRatings: {type: Number, required: true},
+            dateCreated: {type: Date, default: Date.now},
+            lastLogin: {type: Date, default: Date.now}
+
+
+
+
+
+
             if (err) {
                 res.send(err);
             } else {
@@ -41,4 +58,5 @@ exports.login = function(req, res, next) {
                 }
             }
         });
+    }
 }
